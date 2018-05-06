b2fc60d66e9930f909164480dd0fbba7ca149057
bjohnson <bkj.322@gmail.com>
2018-05-06 18:43:41 -0400

    testin sgdr

--- examples/cifar/cifar10.sh
+++ examples/cifar/cifar10.sh
@@ -6,7 +6,9 @@ mkdir -p results
 
 python cifar10.py \
     --epochs 150 \
+    --extra 0 \
+    --burnout 0 \
     --lr-schedule sgdr \
     --lr-max 0.1 \
     --download \
-    --seed 123 > results/cifar10-sgdr-150.jl
+    --seed 123 > results/cifar10-sgdr-150-2.jl
